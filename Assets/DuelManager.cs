70daef3ddca2f6906b4b3bde2e4ce58a7eeb3dcd
memosunoyunlari <93601245+memosunoyunlari@users.noreply.github.com>
2021-12-22 18:13:59 +0300

    3

--- Assets/DuelManager.cs
+++ Assets/DuelManager.cs
@@ -4,19 +4,95 @@ using UnityEngine;
 
 public class DuelManager : MonoBehaviour
 {
+    [SerializeField] GameObject normalBullet;
+
+    private int placeIndex = -1;
     
-    void Start()
+    private bool spawnCondition;
+
+    private void Awake()
     {
-        //spawn arealarýn aralýklarý: 
+        spawnCondition = true;
+        StartCoroutine("StartCountdown");
+    }
+
+    private void Update()
+    {
+        if (placeIndex == 0 && spawnCondition == true)
+        {
+            float xTransform = Random.Range(-7.0f, -1.0f);
+            float yTransform = Random.Range(2.0f, 5.0f);
+
+            Vector2 bulletSpawnLoc = new Vector2(xTransform, yTransform);
+
+            Instantiate(normalBullet, bulletSpawnLoc, Quaternion.identity);
+            spawnCondition = false;
+
+        }
+
+        else if (placeIndex == 1 && spawnCondition == true)
+        {
+            float xTransform = Random.Range(1f, 7f);
+            float yTransform = Random.Range(2f, 5f);
+
+            Vector2 bulletSpawnLoc = new Vector2(xTransform, yTransform);
+
+            Instantiate(normalBullet, bulletSpawnLoc, Quaternion.identity);
+            spawnCondition = false;
+        }
+
+        else if (placeIndex == 2 && spawnCondition == true)
+        {
+
+            float xTransform = Random.Range(-7f, -1f);
+            float yTransform = Random.Range(-3f, 0f);
+
+            Vector2 bulletSpawnLoc = new Vector2(xTransform, yTransform);
+
+            Instantiate(normalBullet, bulletSpawnLoc, Quaternion.identity);
+            spawnCondition = false;
+        }
+
+        else if (placeIndex == 3 && spawnCondition == true)
+        {
+
+            float xTransform = Random.Range(1f, 7f);
+            float yTransform = Random.Range(-3f, 0f);
+
+            Vector2 bulletSpawnLoc = new Vector2(xTransform, yTransform);
+
+            Instantiate(normalBullet, bulletSpawnLoc, Quaternion.identity);
+            spawnCondition = false;
+        }
+        else return;
+
+        
+
+        //spawn arealarn aralklar: 
         //area0: x: -7 to -1, y: 5 to 2
         //area1: x: 7 to 1, y: 5 to 2
         //area2: x: -7 to -1, y: 0 to -3
         //area3: x: 7 to 1, y: 0 to -3
+
+
+
+
+
     }
 
 
-    void Update()
+    IEnumerator StartCountdown()
     {
-        
+        yield return new WaitForSeconds(1);
+        Debug.Log("3");
+        yield return new WaitForSeconds(1);
+        Debug.Log("2");
+        yield return new WaitForSeconds(1);
+        Debug.Log("1");
+        yield return new WaitForSeconds(1);
+
+        placeIndex = Random.Range(0, 3);
+
     }
+
 }
--- Assets/NormalBullet.cs
+++ Assets/NormalBullet.cs
@@ -4,8 +4,52 @@ using UnityEngine;
 
 public class NormalBullet : MonoBehaviour
 {
+    private bool spawnCondition;
+    private void Awake()
+    {
+        float xTransform = transform.position.x;
+        float yTransform = transform.position.y;
+        if ((-7 <= xTransform && xTransform <= -1) && (2 <= yTransform && yTransform <= 5))
+        {
+
+            int spawnIndex = Random.Range(1, 3);
+            if (spawnIndex == 1 && spawnCondition)
+            {
+                //do shit
+            }
+            if (spawnIndex == 1 && spawnCondition)
+            {
+                //do shit
+
+            }
+
+
+            if (spawnIndex == 1 && spawnCondition)
+            {
+                //do shit
+
+
+            }
+
+            if (spawnIndex == 1 && spawnCondition)
+            {
+                //do shit
+
+            }
+
+        }
+
+        //spawn arealarn aralklar: 
+        //area0: x: -7 to -1, y: 5 to 2
+        //area1: x: 7 to 1, y: 5 to 2
+        //area2: x: -7 to -1, y: 0 to -3
+        //area3: x: 7 to 1, y: 0 to -3
+
+        
+    }
     private void OnMouseUpAsButton()
     {
         Destroy(this.gameObject);
     }
+
 }
