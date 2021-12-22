1796a0171fb7e5920d85afdd3ccea497907bccb2
memosunoyunlari <93601245+memosunoyunlari@users.noreply.github.com>
2021-12-22 17:08:37 +0300

    2

---
+++ Assets/DuelManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuelManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        //spawn arealarýn aralýklarý: 
+        //area0: x: -7 to -1, y: 5 to 2
+        //area1: x: 7 to 1, y: 5 to 2
+        //area2: x: -7 to -1, y: 0 to -3
+        //area3: x: 7 to 1, y: 0 to -3
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
