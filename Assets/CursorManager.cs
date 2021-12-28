edca6c6f37717572b3b46cb17339306d0ffe9f87
memosunoyunlari <93601245+memosunoyunlari@users.noreply.github.com>
2021-12-28 18:42:07 +0300

    18

--- Assets/CursorManager.cs
+++ Assets/CursorManager.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class CursorManager : MonoBehaviour
 {
-    public Texture2D cursorArrow;
+    public Texture2D cursorTexture;
+    private Vector2 cursorHotspot;
 
     private void Start()
     {
-        Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+        cursorHotspot = new Vector2(cursorTexture.width / 2, cursorTexture.height / 2);
+        Cursor.SetCursor(cursorTexture, cursorHotspot, CursorMode.ForceSoftware);
     }
 }
