c4290838052090cccf27b16e68c51b35dc3a179d
memosunoyunlari <93601245+memosunoyunlari@users.noreply.github.com>
2021-12-29 03:52:08 +0300

    23

--- Assets/Scripts/Additional Managers/TarAndFeatherManager.cs
+++ Assets/Scripts/Additional Managers/TarAndFeatherManager.cs
@@ -7,36 +7,42 @@ public class TarAndFeatherManager : MonoBehaviour
     [SerializeField] GameObject cursorManager;
     [SerializeField] Texture2D tarredAndFeatherdCursor;
     [SerializeField] Texture2D normalCursor;
+    private GameObject bullet;
     private RaycastHit2D tarAndFeatherHit;
     private Vector2 mousePos;
-    
-    
-
+    private Vector2 cursorHotspot;
 
    
+
+
+    private void Start()
+    {
+        cursorHotspot = new Vector2(normalCursor.width / 2, normalCursor.height / 2);
+    }
+
+
 
 
     void Update()
     {
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         tarAndFeatherHit = Physics2D.Raycast(mousePos, Vector2.zero);
-        
-        if(tarAndFeatherHit.collider.CompareTag("TarAndFeather"))
+
+        if (tarAndFeatherHit.collider.CompareTag("TarAndFeather"))
         {
-            StartCoroutine("tarAndFeatherCursor");
+            Cursor.SetCursor(tarredAndFeatherdCursor, cursorHotspot, CursorMode.ForceSoftware);
         }
+        else return;
+        
 
     }
 
 
     IEnumerator tarAndFeatherCursor()
     {
-        cursorManager.GetComponent<CursorManager>().cursorTexture = tarredAndFeatherdCursor;
-        var bullet = GameObject.FindGameObjectWithTag("Bullet");
-        bullet.GetComponent<CircleCollider2D>().enabled = false;
-        yield return new WaitForSeconds(0.5f);
-        bullet.GetComponent<CircleCollider2D>().enabled = true;
-        cursorManager.GetComponent<CursorManager>().cursorTexture = normalCursor;
+        Cursor.SetCursor(tarredAndFeatherdCursor, cursorHotspot, CursorMode.ForceSoftware);
+        yield return new WaitForSeconds(1);
+        Cursor.SetCursor(normalCursor, cursorHotspot, CursorMode.ForceSoftware);
 
     }
 }
