7caa8a05b51b0061909182045b6ea18e09622252
memosunoyunlari <93601245+memosunoyunlari@users.noreply.github.com>
2022-01-05 17:14:32 +0300

    46

--- Assets/Scripts/Main Managers/EveningManager.cs
+++ Assets/Scripts/Main Managers/EveningManager.cs
@@ -1,24 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class EveningManager : MonoBehaviour
 {
 
-    ////Options Panel
-    //[SerializeField] GameObject OptionsPanel;
-    ////Options Texts
-    [Header("Options Panel Variables")]
+ 
+    [Header("Main Options Panel Variables")]
     [SerializeField] GameObject optionsFirstBit;
-    
-    [SerializeField] GameObject SecondBit;
+    [SerializeField] GameObject optionsSecondBit;
+    bool mainSecondBitCon = true;
+    bool mainOptionsCon = false;
+    [SerializeField] GameObject pressAnyKeyText;
+    [SerializeField] GameObject buttonOne;
+    [SerializeField] GameObject buttonTwo;
 
-    //[SerializeField] GameObject Path1Panel;
+    // optionOnePanel
+    [SerializeField] GameObject optionOnePanel;
+    bool pathOneSecondBitCon = false;
+    [SerializeField] GameObject pathOneFirstBit;
+    [SerializeField] GameObject pathOneSecondBit;
+    // optionTwoPanel
+    [SerializeField] GameObject optionTwoPanel;
+    bool pathTwoSecondBitCon = false;
+    [SerializeField] GameObject pathTwoFirstBit;
+    [SerializeField] GameObject pathTwoSecondBit;
 
-    //[SerializeField] GameObject Path2Panel;
+    bool nextScene = false;
 
-    //Texts
+    [Header("Tournament Manager")]
+    [SerializeField] GameObject tournamentManager;
 
     private void Awake()
     {
@@ -27,17 +38,60 @@ public class EveningManager : MonoBehaviour
 
     private void Update()
     {
-        
+        if (Input.anyKeyDown && mainSecondBitCon)
+        {
+            optionsSecondBit.GetComponent<Animator>().SetTrigger("FadeIn");
+            mainSecondBitCon = false;
+            mainOptionsCon = true;
+        }
+        else if (Input.anyKeyDown && !mainSecondBitCon && mainOptionsCon)
+        {
+            pressAnyKeyText.GetComponent<Animator>().SetTrigger("FadeOut");
 
-       
+            buttonOne.GetComponent<Animator>().SetTrigger("FadeIn");
+            buttonTwo.GetComponent<Animator>().SetTrigger("FadeIn");
+
+            mainOptionsCon = false;
+        }
+        else if (Input.anyKeyDown && pathOneSecondBitCon)
+        {
+            pathOneSecondBitCon = false;
+            pathOneSecondBit.GetComponent<Animator>().SetTrigger("FadeIn");
+            nextScene = true;
+        }
+        else if(Input.anyKeyDown && pathTwoSecondBitCon)
+        {
+            pathTwoSecondBitCon = false;
+            pathTwoSecondBit.GetComponent<Animator>().SetTrigger("FadeIn");
+            nextScene = true;
+        }
+        
+        else if(Input.anyKeyDown && nextScene)
+        {
+            tournamentManager.GetComponent<TournamentManager>().nextScene();
+        }
     }
 
     IEnumerator OptionsPanelSetter()
     {
         yield return new WaitForSeconds(1);
         optionsFirstBit.GetComponent<Animator>().SetTrigger("FadeIn");
+        pressAnyKeyText.GetComponent<Animator>().SetTrigger("FadeIn");
     }
 
+    public void eveningOneOptionOne()
+    {
+        optionOnePanel.SetActive(true);
+        pathOneSecondBitCon = true;
+        pathOneFirstBit.GetComponent<Animator>().SetTrigger("FadeIn");
+    }
+
+    public void eveningOneoptionTwo()
+    {
+        optionTwoPanel.SetActive(true);
+        pathTwoSecondBitCon = true;
+        pathTwoFirstBit.GetComponent<Animator>().SetTrigger("FadeIn");
+    }
    
 
 }
