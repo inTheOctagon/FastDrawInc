6dcb0ac03de7bee9bf564fb949583beb8e7f2444
memosunoyunlari <93601245+memosunoyunlari@users.noreply.github.com>
2022-01-13 19:25:32 +0300

    72 - Almost done

--- Assets/Scripts/Main Managers/MainMenuManager.cs
+++ Assets/Scripts/Main Managers/MainMenuManager.cs
@@ -6,17 +6,23 @@ using TMPro;
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] TMP_InputField yourName;
+    [SerializeField] GameObject yourNameHere;
     
     //transition
     [SerializeField] GameObject tournamentManager;
     [SerializeField] GameObject fadeInPanel;
+    [SerializeField] GameObject fadeOutPanel;
 
+    private void Start()
+    {
+        StartCoroutine("setTheMenuUp");
+    }
 
     public void signButton()
     {
         if (yourName.text.Length < 1)
         {
-            Debug.Log("your name, sir");
+            StartCoroutine("yourNameHereSir");
         }
 
         else
@@ -25,15 +31,30 @@ public class MainMenuManager : MonoBehaviour
         }
     }
 
+    IEnumerator setTheMenuUp()
+    {
+        yield return new WaitForSeconds(1.5f);
+        fadeOutPanel.GetComponent<Animator>().SetTrigger("FadeOut");
+        yield return new WaitForSeconds(2);
+        fadeOutPanel.SetActive(false);
+    }
+
     IEnumerator weGoIn()
     {
+        
         fadeInPanel.SetActive(true);
         ValueManager.playerName = yourName.text.ToString();
         fadeInPanel.GetComponent<Animator>().SetTrigger("FadeIn");
         yield return new WaitForSeconds(2);
-        Debug.Log("your name " + ValueManager.playerName + "ye");
         yield return new WaitForSeconds(2);
         tournamentManager.GetComponent<TournamentManager>().nextScene();
+    }
+
+    IEnumerator yourNameHereSir()
+    {
+        yourNameHere.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        yourNameHere.SetActive(false);
     }
     
 }
